9705d326ebe817a023ae4528be83754df1a98419
Luiz Guilherme Guerreiro <84469900+Luuizze@users.noreply.github.com>
2023-03-20 21:41:10 -0300

    Create serie.cs

---
+++ serie.cs
@@ -0,0 +1,14 @@
+public class Serie
+{
+    private CatalogoMediador mediador;
+ 
+    public Serie(CatalogoMediador mediador)
+    {
+        this.mediador = mediador;
+    }
+ 
+    public void Reproduzir()
+    {
+        this.mediador.ReproduzirSerie(this);
+    }
+}
